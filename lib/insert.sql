c3e3bbe1267311ea71874e9c7a320e399a4463a0
jastaffen <jack.staffen@gmail.com>
2019-10-15 17:38:28 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "rude", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "female", "speckled", "aloof", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "female", "brown", "despondent", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "female", "brown", "cuddly", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "female", "white", "timid", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "male", "speckled", "total ass", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 8, "female", "brown", "sweet", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6, "female", "brown", "unpredictable", true);
+
+
+-- * Mr. Chocolate
+-- * Rowdy
+-- * Tabitha
+-- * Sergeant Brown
+-- * Melissa
+-- * Grinch
+-- * Wendy
+-- * unnamed (the bear that killed 
+-- Tim didn't have a name; refer back to how to create a record that 
+-- doesn't have one value)
